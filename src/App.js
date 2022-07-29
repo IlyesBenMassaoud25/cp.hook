0f6d74997a968169646decc33850cd143c2da524
IlyesBenMassaoud25 <ilyestrayhard@gmail.com>
2022-07-29 13:34:37 +0200

    checkpoint . route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from 'react';
 
 import { moviesData } from './components/MoviesData';
 import SearchMovie from './components/SearchMovie/SearchMovie';
 import MoviesList from './components/MoviesList';
 import AddMovie from './components/AddMovie/AddMovie';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
+import MoviePage from './components/MoviePage/MoviePage';
 
 function App() {
   const [moviesList, setMoviesList] = useState(moviesData);
@@ -18,21 +19,40 @@ function App() {
   };
 
   return (
-    <div className="App">
+    
+      <BrowserRouter>
+          <div className="App">
+
+      <Routes>
+        <Route exact path="/" element={ 
+        <Fragment>
       <SearchMovie
         setNameSearch={setNameSearch}
         ratingSearch={ratingSearch}
         setRatingSearch={setRatingSearch}
       />
-      <MoviesList
-        moviesList={moviesList}
-        nameSearch={nameSearch}
-        ratingSearch={ratingSearch}
-      />
-      <div style={{ display: 'flex', justifyContent: 'center' }}>
+            <MoviesList
+            moviesList={moviesList}
+            nameSearch={nameSearch}
+            ratingSearch={ratingSearch}
+          />
+          <div style={{ display: 'flex', justifyContent: 'center' }}>
         <AddMovie addNewMovie={addNewMovie} />
       </div>
-    </div>
+        </Fragment>
+      }>
+          
+        </Route>
+        <Route path="/movie/:id" element={<MoviePage movies={moviesData}/>} />
+
+      </Routes>
+      
+      </div>
+    </BrowserRouter>
+    
+
+   
+   
   );
 }
 
--- src/Components/MovieCard/MovieCard.js
+++ src/Components/MovieCard/MovieCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
 import Rate from '../Rate';
-
+import { Link } from 'react-router-dom';
 import './MovieCard.css';
 
 const MovieCard = ({
-  movie: { name, date, rating, image, type, description },
+  movie: { id,name, date, rating, image, type, description },
 }) => {
   return (
+    <Link to={`/movie/${id}` }>
     <div className="card">
       <div className="card_left">
         <img src={image} alt="poster" />
@@ -26,13 +27,14 @@ const MovieCard = ({
         </div>
         <div className="card_right__review">
           <p>{description}</p>
-          <a href="https://www.imdb.com/?ref_=nv_home">About</a>
+          <a href="https://www.imdb.com/?ref_=nv_home">Read More</a>
         </div>
         <div className="card_right__button">
-          <a href="https://www.imdb.com/?ref_=nv_home">WATCH the video </a>
+          <a href="https://www.imdb.com/?ref_=nv_home">WATCH TRAILER</a>
         </div>
       </div>
     </div>
+    </Link>
   );
 };
 
